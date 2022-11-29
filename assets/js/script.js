047472bb0a0bb96bdf4d8a1a9ca5d3e8e4262453
Supersheep50 <jonathan.morrisseyto@gmail.com>
2022-11-29 10:57:26 +0000

    Add buttons to quiz box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 let beginQuiz = document.getElementsByClassName("begin-quiz");
 let gameBox = document.getElementsByClassName("game-box");
 let questionBox = document.getElementsByClassName("question-box")
+let gameQuestions = document.getElementsByClassName("game-questions")
 let answerBox = document.getElementsByClassName("answer-box");
-let
+let buttonA = document.getElementsByClassName("btn-a")
+let buttonB = document.getElementsByClassName("btn-b")
+let buttonC = document.getElementsByClassName("btn-c")
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener("click", beginQuiz, {
+        once: true
+    })
+})
