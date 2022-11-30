cb65e091deb1efddfd80d9bd6cbd66f2c53b3308
Supersheep50 <jonathan.morrisseyto@gmail.com>
2022-11-30 16:14:05 +0000

    Add code to make questions random and not repeat

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,59 @@ let answerBox = document.getElementsByClassName("answer-box");
 let buttonA = document.getElementsByClassName("btn-a")
 let buttonB = document.getElementsByClassName("btn-b")
 let buttonC = document.getElementsByClassName("btn-c")
-let beginQuiz = document.getElementsByClassName("begin-quiz");
 let currentQuestion = null;
+let repeatedQuestion = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.addEventListener("click", beginQuiz, {
+    document.addEventListener("click", startGame, {
         once: true
     })
 })
 
+/** Begin Quiz */
+function startGame() {
+
+    gameBox.style.display = "none";
+    content.style.display = "block";
+    renderNextQuestion();
+
+}
+
+function renderNextQuestion() {
+    resetButtons();
+    if (isMaximumQuestionsLimitReached()) {
+        displayResult();
+    } else {
+        displayQuestion();
+    }
+}
+
+
 /** Shows Questions */
 function displayQuestion() {
-    questions = getRandomQuestion();
-    gameQuestions.innerText = currentQuestion.question;
+    currentQuestion = getRandomQuestion();
+    questionBox.innerText = currentQuestion.question;
     displayAnswer();
     handleClickEvent();
+}
+
+/**Shows Answers */
+
+function displayAnswer() {
+    buttonA.innerText = currentQuestion.options[0];
+    buttonB.innerText = currentQuestion.options[1];
+    buttonC.innerText = currentQuestion.options[2];
+
+}
+
+function getRandomQuestion() {
+    currentQuestion = questions[Math.floor(Math.random() * questions.length)];
+    if (isMaximumQuestionsLimitReached()) {
+        return false;
+    } else if (repeatedQuestion.indexOf(currentQuestion) >= 0) {
+        return getRandomQuestion();
+    } else {
+        repeatedQuestion.push(currentQuestion);
+        return currentQuestion;
+    }
 }
