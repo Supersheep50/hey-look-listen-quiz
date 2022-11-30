8a529be32cb45ac1c7299c820083f868973b05f5
Supersheep50 <jonathan.morrisseyto@gmail.com>
2022-11-30 14:53:46 +0000

    Add Questions and JS code for dsiplaying Question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-let beginQuiz = document.getElementsByClassName("begin-quiz");
+import {
+    questions
+} from ".trivia-questions.js";
+
 let gameBox = document.getElementsByClassName("game-box");
 let questionBox = document.getElementsByClassName("question-box")
 let gameQuestions = document.getElementsByClassName("game-questions")
@@ -6,10 +9,19 @@ let answerBox = document.getElementsByClassName("answer-box");
 let buttonA = document.getElementsByClassName("btn-a")
 let buttonB = document.getElementsByClassName("btn-b")
 let buttonC = document.getElementsByClassName("btn-c")
-
+let beginQuiz = document.getElementsByClassName("begin-quiz");
+let currentQuestion = null;
 
 document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener("click", beginQuiz, {
         once: true
     })
 })
+
+/** Shows Questions */
+function displayQuestion() {
+    questions = getRandomQuestion();
+    gameQuestions.innerText = currentQuestion.question;
+    displayAnswer();
+    handleClickEvent();
+}
---
+++ assets/trivia-questions.js
@@ -0,0 +1,17 @@
+export let questions = [{
+        "question": "When was Metal Gear Solid Released?",
+        "choices": ["1998", "2001", "2004"],
+        "answer": "1998"
+    }, {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+
+
+
+
+
+
+];
