470d700774d5535d13fe7e1e006071ff6d829cac
Supersheep50 <jonathan.morrisseyto@gmail.com>
2022-12-01 10:58:10 +0000

    Add new questions to quiz

--- assets/trivia-questions.js
+++ assets/trivia-questions.js
@@ -5,6 +5,114 @@ export let questions = [{
     }, {
         "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
         "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "Link"
+    }
+
+    {
+        "question": "What is the worst game ever made?",
+        "choices": ["Far Cry 5", "Far Cry 5", "Far Cry 5"],
+        "answer": "Far Cry 5"
+    }
+
+    {
+        "question": "What is Mario's brothers name?",
+        "choices": ["Luigi", "Tony", "Sergio"],
+        "answer": "Luigi"
+    }
+
+    {
+        "question": "Who is the best looking member of HLL?",
+        "choices": ["Owen", "Liam", "Kev"],
+        "answer": "Jon"
+    }
+
+    {
+        "question": "In what city is Resident Evil 2 set?",
+        "choices": ["Badger City", "Fox City", "Racoon City"],
+        "answer": "Racoon City"
+    }
+
+    {
+        "question": "What is the name of Nintendo's orignal handheld console?",
+        "choices": ["PSP", "Steam Deck", "Gameboy"],
+        "answer": "Gameboy"
+    }
+
+    {
+        "question": "In the Elder Scrolls, what is the name of the capital city of Tamriel?",
+        "choices": ["Skyrim", "Cyrodiil City", "Cork"],
+        "answer": "Cyrodiil City"
+    }
+
+    {
+        "question": "What is the name of Geralt's horse in The Witcher 3?",
+        "choices": ["Sarah", "Jessica", "Roach"],
+        "answer": "Roach"
+    }
+
+    {
+        "question": "",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
+        "answer": "1998"
+    }
+
+    {
+        "question": "What is the name of the main protagonist in The Legend of Zelda: Ocarina of Time?",
+        "choices": ["Zelda", "Link", "Jeff"],
         "answer": "1998"
     }
 
