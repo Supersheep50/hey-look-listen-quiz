a4be23231333fe9f13db7cacea770f3e2f2828bf
Supersheep50 <jonathan.morrisseyto@gmail.com>
2022-12-06 16:37:57 +0000

    Add functionality for mouse use

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 /* jshint esversion: 11 */
 document.getElementById("question-counter").innerText = questions.length;
 
+let startGame = document.getElementsByClassName("begin-quiz")[0];
 let questionBox = document.getElementsByClassName("question-box")[0];
 let buttonA = document.getElementsByClassName("btn-a")[0];
 let buttonB = document.getElementsByClassName("btn-b")[0];
 let buttonC = document.getElementsByClassName("btn-c")[0];
-let startGame = document.getElementsByClassName("begin-quiz")[0];
+let answerButtons = document.getElementsByClassName("answer-btn")
+let actionButtons = document.getElementsByClassName("action-btn")
+let playAgainButton = document.getElementsByClassName("play-again")[0];
 let currentQuestion = null;
 let repeatedQuestion = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     startGame.addEventListener("click", beginQuiz, {
         once: true
-    })
+    });
 })
 
 /** Begin Quiz */
@@ -29,6 +32,7 @@ function isMaximumQuestionsLimitReached() {
 
 /** Renders the next question */
 function renderNextQuestion() {
+
     if (isMaximumQuestionsLimitReached()) {
         displayResult();
     } else {
@@ -41,7 +45,7 @@ function displayQuestion() {
     currentQuestion = getRandomQuestion();
     questionBox.innerText = currentQuestion.question;
     displayAnswers();
-    handleClickEvent();
+
 }
 
 /**Shows Answers */
@@ -63,5 +67,42 @@ function getRandomQuestion() {
     } else {
         repeatedQuestion.push(currentQuestion);
         return currentQuestion;
+    }
+}
+
+function handleMouseEvent() {
+    for (let answerButton of answerButtons) {
+        answerButton.addEventListener("mousedown", checkAnswer);
+        answerButton.addEventListener("mouseup", renderNextQuestion);
+    }
+    for (let actionButton of actionButtons) {
+        actionButton.addEventListener("mousedown", styleActionButton);
+        actionButton.addEventListener("mouseup", unStyleActionButton);
+    }
+}
+
+function checkAnswer() {
+    let answer = currentQuestion.answer;
+    let response = this.innerText;
+    if (answer === response) {
+        this.style.backgroundColor = "gold"
+        incrementScore();
+    } else {
+        this.style.backgroundColor = "black"
+    }
+}
+
+function incrementScore() {
+    score.innerText = ++scoreCount;
+}
+
+function displayActionButtons() {
+    for (let answerButton of answerButtons) {
+        answerButton.style.display = "none";
+    }
+    for (let actionButton of actionButtons) {
+        actionButton.style.display = "inline-block";
+
+        playAgainButton.addEventListener("click", newGame)
     }
 }
